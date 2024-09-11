a731013aaa885dd3d86d46f719fedc891356a8e1
Arron Linton <arronlinton@Arrons-MacBook-Pro.local>
2024-09-11 16:46:09 -0400

    content update

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -22,7 +22,7 @@ const Contact = () => (
         </div>
       </header>
       <div className="email-at">
-        <p>Feel free to get in touch. You can email me at: </p>
+        <p>You can email me at: </p>
         <EmailLink />
       </div>
       <ContactIcons />
