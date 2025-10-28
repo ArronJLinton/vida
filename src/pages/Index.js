edf37272ef6d321a7afda2b4ea2b9125d4c85cb8
Arron Linton <lintonarron@gmail.com>
2025-10-28 09:33:53 -0400

    updated projects

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -6,28 +6,23 @@ import Main from '../layouts/Main';
 const Index = () => (
   <Main
     description={
-      "Arron J. Linton's personal website. New York City based Software Engineer, "
-      + 'Lead Instructor, and Rutgers University Alumni.'
+      "Arron J. Linton's personal website. New York City based Software Engineer, Lead Instructor, and Rutgers University Alumni."
     }
   >
     <article className="post" id="index">
       <header>
         <div className="title">
           <h2>
-            <Link to="/">About this site</Link>
+            <Link to="/">Welcome</Link>
           </h2>
-          <p>
-            A beautiful, responsive, statically-generated, react application
-            written with modern Javascript.
-          </p>
         </div>
       </header>
       <p>
         {' '}
         Welcome to my website. Please feel free to read more{' '}
         <Link to="/about">about me</Link>, or you can check out my{' '}
-        <Link to="/resume">resume</Link>, <Link to="/projects">projects</Link>, or{' '}
-        <Link to="/contact">contact</Link> me.
+        <Link to="/resume">resume</Link>, <Link to="/projects">projects</Link>,
+        or <Link to="/contact">contact</Link> me.
       </p>
       {/* <p>
         {' '}
